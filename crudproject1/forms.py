a2dce6f97bbb1d8fe761788c0fa90d43ff7eeff7
NDERI <tnderi373@gmail.com>
2023-07-28 09:29:05 +0300

    initial commit

---
+++ crudproject1/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from crudproject1.models import Student
+
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['firstname', 'lastname', 'email']
